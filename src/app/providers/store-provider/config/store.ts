379a31658114086d715a90062ab188963effce3f
KuzMich1994 <drumer19940310@mail.ru>
2023-04-08 12:49:55 +0300

    fix store provider

--- src/app/providers/store-provider/config/store.ts
+++ src/app/providers/store-provider/config/store.ts
@@ -4,8 +4,12 @@ import { userReducer } from 'entities/user';
 import { StateSchema } from './state-schema';
 import { createReducerManager } from './reducer-manager';
 
-export const createReduxStore = (initialState?: StateSchema) => {
+export const createReduxStore = (
+  initialState?: StateSchema,
+  asyncReducers?: ReducersMapObject<StateSchema>,
+) => {
   const rootReducer: ReducersMapObject<StateSchema> = {
+    ...asyncReducers,
     counter: counterReducer,
     user: userReducer,
   };
--- src/app/providers/store-provider/ui/store-provider.tsx
+++ src/app/providers/store-provider/ui/store-provider.tsx
@@ -2,18 +2,24 @@ import { ReactNode, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { createReduxStore } from 'app/providers/store-provider/config/store';
 import { StateSchema } from 'app/providers/store-provider/config/state-schema';
-import { AnyAction, DeepPartial, ThunkMiddleware } from '@reduxjs/toolkit';
+import {
+  AnyAction, DeepPartial, ReducersMapObject, ThunkMiddleware,
+} from '@reduxjs/toolkit';
 import { ToolkitStore } from '@reduxjs/toolkit/dist/configureStore';
 
 interface StoreProviderProps {
   children: ReactNode;
   initialState?: DeepPartial<StateSchema>;
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
 }
 
 let createdStore: ToolkitStore<StateSchema, AnyAction, [ThunkMiddleware<StateSchema, AnyAction, undefined>]>;
 
-export function StoreProvider({ children, initialState }: StoreProviderProps): JSX.Element {
-  const store = createReduxStore(initialState as StateSchema);
+export function StoreProvider({ children, initialState, asyncReducers }: StoreProviderProps): JSX.Element {
+  const store = createReduxStore(
+    initialState as StateSchema,
+    asyncReducers as ReducersMapObject<StateSchema>,
+  );
 
   useEffect(() => {
     createdStore = store;
