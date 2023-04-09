732ad5c048b0fb1cfe624c6eaf8defbb9f6114ae
KuzMich1994 <drumer19940310@mail.ru>
2023-04-09 21:38:13 +0300

    add profile page, profile slice, optimization render

--- src/shared/config/storybook/store-decorator/store-decorator.tsx
+++ src/shared/config/storybook/store-decorator/store-decorator.tsx
@@ -2,9 +2,11 @@ import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/store-provider';
 import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { loginReducer } from 'features/auth-by-username/model/slice/login-slice';
+import { profileReducer } from 'entities/profile';
 
 const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
   login: loginReducer,
+  profile: profileReducer,
 };
 
 export const StoreDecorator = (
