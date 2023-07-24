f82545ebed9b653cab16ed64b9b3f87a3fcd41b9
KuzMich1994 <drumer19940310@mail.ru>
2023-07-24 12:35:33 +0300

    add private route with required roles

--- src/pages/admin-panel-page/ui/admin-panel-page.tsx
+++ src/pages/admin-panel-page/ui/admin-panel-page.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/class-names/class-names';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import Page from 'widgets/page/page';
-import s from './admin-panel-page.module.scss';
 
 interface AdminPanelPageProps {
   className?: string;
@@ -12,7 +11,7 @@ function AdminPanelPage({ className }: AdminPanelPageProps): JSX.Element {
   const { t } = useTranslation();
 
   return (
-    <Page className={classNames(s.adminPanelPage, {}, [className])}>
+    <Page className={classNames('', {}, [className])}>
       {t('Админ панель')}
     </Page>
   );
--- src/pages/forbidden-page/ui/forbidden-page.tsx
+++ src/pages/forbidden-page/ui/forbidden-page.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/class-names/class-names';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import Page from 'widgets/page/page';
-import s from './forbidden-page.module.scss';
 
 interface ForbiddenPageProps {
   className?: string;
@@ -12,7 +11,7 @@ function ForbiddenPage({ className }: ForbiddenPageProps): JSX.Element {
   const { t } = useTranslation();
 
   return (
-    <Page className={classNames(s.forbiddenPage, {}, [className])}>
+    <Page className={classNames('', {}, [className])}>
       {t('У вас нет доступа к этой странице')}
     </Page>
   );
