60ae87c9a174b90638ee3797804c7a833bae1768
KuzMich1994 <drumer19940310@mail.ru>
2023-07-26 20:03:19 +0300

    add popover, add notifications

---
+++ src/entities/notification/index.ts
@@ -0,0 +1,7 @@
+import NotificationList from './ui/notification-list/notification-list';
+import { NotificationSchema } from './model/types/notification-schema';
+
+export {
+  NotificationList,
+  type NotificationSchema,
+};
