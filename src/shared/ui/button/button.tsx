70ed59427d6687b3cc837d00654aa595dea9284e
KuzMich1994 <drumer19940310@mail.ru>
2023-08-01 10:23:41 +0300

    add docs

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -19,9 +19,21 @@ export enum ButtonSize {
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
+  /**
+   * Тема кнопки. Отвечает за визуал (в рамке, без стилей, противоположный теме приложения цвет и тд)
+   */
   theme?: ButtonTheme;
+  /**
+   * Флаг, делающий кнопку квадратной
+   */
   square?: boolean;
+  /**
+   * Размер кнопки в соответствии с дизайн системой
+   */
   buttonSize?: ButtonSize;
+  /**
+   * Увеличивает кнопку на всю свободную ширину
+   */
   fullWidth?: boolean;
 }
 
